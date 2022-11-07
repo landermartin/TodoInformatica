a5fab68a597ec9fbd9ec426b41df1100d745353b
Lander Martin <lander.martin@opendeusto.es>
2022-11-07 11:15:06 +0100

    arreglado urlas ya entran tambin nums

--- appTodoInformatica/urls.py
+++ appTodoInformatica/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
  path('', views.index_main, name='index'),
- path('articulos/',views.index_articulos,name='articulos'),
+ path('articulo/',views.index_articulos,name='articulos'),
  path('categoria/', views.index_categoria, name='categorias'),
- path('tienda/', views.index_Tiendas, name='tiendas'),
+ path('tienda/', views.index_Tiendas, name='tienda'),
  path('tienda/<int:tienda_id>', views.show_Tienda, name='tienda'),
  path('articulo/<int:articulo_id>', views.show_articulo, name='articulo'),
  path('categoria/<int:categoria_id>', views.show_categoria, name='categoria'),
