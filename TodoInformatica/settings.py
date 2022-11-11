55b3128a7dd4f8fd30c4c4a907595f824d986700
Imanol Gutierrez <developer@imanol.org>
2022-11-11 09:48:32 +0100

    para oier

--- TodoInformatica/settings.py
+++ TodoInformatica/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-04gh-g*q#*^t2%f^(j2i&efmc%0g$6!3v-3h06!_=tppr7!099'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -137,7 +137,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-"""
+
 # For production
 SECRET_KEY = "cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag"
 DEBUG = False
@@ -147,4 +147,3 @@ SESSION_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 518400
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-"""
