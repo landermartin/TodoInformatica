48e455b82e1939f82bb2cbb226a8af89e533c455
Imanol Gutierrez <developer@imanol.org>
2022-12-09 19:08:10 +0100

    Pequeños cambios

--- appTodoInformatica/js/index.js
+++
@@ -1,6 +0,0 @@
-function generarCodigo(max) {
-    let r='';
-    for (let i=0;i<max/13;i++)
-      r+=(Math.random()+1).toString(16).substring(2);
-    return r.substring(0,max).toUpperCase();
-  }
