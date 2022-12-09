5abcc809cce44c691ddb6fb2bd07f7566977ae82
Lander Martin <lander.martin@opendeusto.es>
2022-12-09 16:30:54 +0100

    creo que corregido

--- appTodoInformatica/js/index.js
+++ appTodoInformatica/js/index.js
@@ -4,18 +4,3 @@ function generarCodigo(max) {
       r+=(Math.random()+1).toString(16).substring(2);
     return r.substring(0,max).toUpperCase();
   }
-
-  $(document).ready(function(){
-      $("#imagenDados").click(function(){
-        if (Math.floor(Math.random() * 100) <= 20) {
-          const cod = generarCodigo(6);
-          $("#codigoPromocion").text("Enhorabuena! Tu código es #" + cod); // establecer mensaje
-          $("#codigoPromocion").show(); // mostrar mensaje
-          $("#imagenDados").hide(); // quitar los dados
-        } else {
-          $("#codigoPromocion").text("Vaya! No ha habido suerte :("); // establecer mensaje
-          $("#codigoPromocion").show(); // mostrar mensaje
-          $("#imagenDados").hide(); // quitar los dados
-        }
-      });
-  });
